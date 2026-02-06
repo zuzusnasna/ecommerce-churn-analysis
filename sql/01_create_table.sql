394aca1ca1784487467ca584d735fe3dff05796c
zuzusnasna <jsmh991102@gmail.com>
2026-02-06 17:08:31 +0900

    feat: create raw transaction table

--- sql/01_create_table.sql
+++ sql/01_create_table.sql
@@ -1,1 +1,11 @@
 -- create online retail tables
+-- raw transaction table
+CREATE TABLE raw_transactions (
+    invoice_no     VARCHAR(20),
+    customer_id    VARCHAR(20),
+    invoice_date   TIMESTAMP,
+    quantity       INT,
+    unit_price     DECIMAL(10,2),
+    country        VARCHAR(50)
+);
+
